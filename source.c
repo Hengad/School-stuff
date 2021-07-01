24741d87b519f688ded2fa92b0491353f0337625
Hengad <henry.jaakkola@hotmail.com>
2021-07-01 05:03:04 +0300

    Add files via upload

---
+++ source.c
@@ -0,0 +1,39 @@
+#include "source.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char *mystrcat(char *dest, const char *src)
+{
+	int size = 1;
+	const char *counter = dest;
+	while (*counter++)
+		size++;
+	counter = src;
+	while (*counter++)
+		size++;
+
+	char* buffer = malloc(strlen(dest) + 1);
+	strcpy(buffer, dest);
+
+	char *newDest = realloc(dest, size * sizeof(char));
+	dest = newDest;
+	
+	for (int i = 0; i < (int)strlen(buffer); i++)
+		*newDest++ = buffer[i];
+	while (*src)
+		*newDest++ = *src++;
+
+	*newDest = 0;
+	free(buffer);
+
+	return dest;
+}
+
+
+/*  EXAMPLE USAGE
+
+	char *str = malloc(7);
+	strcpy(str, "Aatami");
+	str = mystrcat(str, "Beetami");
+*/
